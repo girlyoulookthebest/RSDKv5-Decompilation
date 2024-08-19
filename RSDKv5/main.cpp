ef4ab01d0153f786aa8ec4fd451fc1611f6f92f2
smb123w64gb <smb123w64gb@gmail.com>
2024-08-19 04:44:26 -0700

    Exit Callback(We can finaly go home!)

--- RSDKv5/main.cpp
+++ RSDKv5/main.cpp
@@ -7,6 +7,42 @@
     PSP_MODULE_INFO("Sonic_Mania", 0, 1, 1);
 
     PSP_MAIN_THREAD_ATTR(THREAD_ATTR_USER | THREAD_ATTR_VFPU);
+
+    /* Exit callback */
+int exit_callback(int arg1, int arg2, void *common)
+{
+	sceKernelExitGame();
+
+	return 0;
+}
+
+/* Callback thread */
+int CallbackThread(SceSize args, void *argp)
+{
+	int cbid;
+
+	cbid = sceKernelCreateCallback("Exit Callback", (void *) exit_callback, NULL);
+	sceKernelRegisterExitCallback(cbid);
+
+	sceKernelSleepThreadCB();
+
+	return 0;
+}
+
+/* Sets up the callback thread and returns its thread id */
+int SetupCallbacks(void)
+{
+	int thid = 0;
+
+	thid = sceKernelCreateThread("update_thread", CallbackThread, 0x11, 0xFA0, 0, 0);
+	if(thid >= 0)
+	{
+		sceKernelStartThread(thid, 0, 0);
+	}
+
+	return thid;
+}
+
 #endif
 
 #if RETRO_STANDALONE
@@ -85,6 +121,7 @@ void android_main(struct android_app *ap)
 #else
 int32 main(int32 argc, char *argv[]) { 
     #ifdef __PSP__
+    SetupCallbacks();
     PSP_HEAP_SIZE_KB(-128);
 
     #endif
